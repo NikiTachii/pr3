44309bbe39bd85fe010c7728ccc77439ffd74ac2
nikita <nikitis211@mail.ru>
2024-09-05 12:39:49 +0300

    sec commit

--- script.js
+++ script.js
@@ -34,11 +34,11 @@ let result = (d + e < 4) ? 'Мало' : 'Много';
 alert(`${result}`);
 
 // //Задание 4
-// let message = 
-// (login == 'Сотрудник') ? 'Привет' :
-// (login == 'Директор') ? 'Здравствуйте' :
-// (login == '') ? 'Нет логина' : '';
-// alert(`${message}`);
+let log = prompt('Введите login:');
+let message = log == 'Сотрудник' ? 'Привет' :
+    log == 'Директор' ? 'Здравствуйте' :
+        log == '' ? 'Нет логина' : '';
+alert(message);
 
 //Задание 5
 let login = prompt("Введите логин");
